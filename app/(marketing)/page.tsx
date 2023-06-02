b657d68ab1c8f2c42789469d5ca3aca04ec1c6d5
Paweł Biegun <biegunpawel900@gmail.com>
2023-06-02 09:00:25 -0700

    save

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -69,7 +69,7 @@ export default async function IndexPage() {
                 <path d="M10 18h4"></path>
               </svg>
               <div className="space-y-2">
-                <h3 className="font-bold">Verified charities</h3>
+                <h3 className="font-bold">Trust</h3>
                 <p className="text-sm">
                   Only verified charities can use CollectionBox
                 </p>
@@ -91,7 +91,7 @@ export default async function IndexPage() {
                 <polyline points="20 6 9 17 4 12"></polyline>
               </svg>
               <div className="space-y-2">
-                <h3 className="font-bold">Easy to use</h3>
+                <h3 className="font-bold">Ease of use</h3>
                 <p className="text-sm text-muted-foreground">
                   Our intuitive and modern UI creates a positive user experience
                 </p>
@@ -105,9 +105,32 @@ export default async function IndexPage() {
                 <path d="M7.07 14.94c-1.66 0-3 1.35-3 3.02 0 1.33-2.5 1.52-2 2.02 1.08 1.1 2.49 2.02 4 2.02 2.2 0 4-1.8 4-4.04a3.01 3.01 0 0 0-3-3.02z"></path>
               </svg>
               <div className="space-y-2">
-                <h3 className="font-bold">Customizable</h3>
+                <h3 className="font-bold">Customizability</h3>
                 <p className="text-sm text-muted-foreground">
                   Make your collection box unique using images, videos and text.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="relative overflow-hidden rounded-lg border bg-background p-2">
+            <div className="flex h-[180px] flex-col justify-between rounded-md p-6">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="h-12 w-12"
+              >
+                <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path>
+              </svg>
+              <div className="space-y-2">
+                <h3 className="font-bold">Going viral</h3>
+                <p className="text-sm text-muted-foreground">
+                  CollectionBox is optimized for users to create content that
+                  can go viral on social media.
                 </p>
               </div>
             </div>
@@ -119,25 +142,26 @@ export default async function IndexPage() {
                 fill="none"
                 stroke="currentColor"
                 strokeWidth="1"
-                className="h-12 w-12 fill-current"
+                className="h-12 w-12 "
               >
                 <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
               </svg>
               <div className="space-y-2">
                 <h3 className="font-bold">Security</h3>
                 <p className="text-sm text-muted-foreground">
-                  The application is secured using Auth.js
+                  The application is secured using Auth.js and uses security
+                  best practices
                 </p>
               </div>
             </div>
           </div>
           <div className="relative overflow-hidden rounded-lg border bg-background p-2">
             <div className="flex h-[180px] flex-col justify-between rounded-md p-6">
-              <svg viewBox="0 0 24 24" className="h-12 w-12 fill-current">
+              <svg viewBox="0 0 24 24" className="h-12 w-12">
                 <path d="M13.976 9.15c-2.172-.806-3.356-1.426-3.356-2.409 0-.831.683-1.305 1.901-1.305 2.227 0 4.515.858 6.09 1.631l.89-5.494C18.252.975 15.697 0 12.165 0 9.667 0 7.589.654 6.104 1.872 4.56 3.147 3.757 4.992 3.757 7.218c0 4.039 2.467 5.76 6.476 7.219 2.585.92 3.445 1.574 3.445 2.583 0 .98-.84 1.545-2.354 1.545-1.875 0-4.965-.921-6.99-2.109l-.9 5.555C5.175 22.99 8.385 24 11.714 24c2.641 0 4.843-.624 6.328-1.813 1.664-1.305 2.525-3.236 2.525-5.732 0-4.128-2.524-5.851-6.594-7.305h.003z" />
               </svg>
               <div className="space-y-2">
-                <h3 className="font-bold">Payments</h3>
+                <h3 className="font-bold">Easy payments</h3>
                 <p className="text-sm text-muted-foreground">
                   Payments are securely managed end to end using Stripe
                 </p>
