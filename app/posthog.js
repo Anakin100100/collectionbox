4ad56e213ced80213162f8f3df9ce4604ee54999
Paweł Biegun <biegunpawel900@gmail.com>
2023-06-06 10:26:17 -0700

    save

--- app/posthog.js
+++ app/posthog.js
@@ -1,6 +1,9 @@
 import { PostHog } from "posthog-node"
 
 export default function PostHogClient() {
+  console.log("posthog")
+  console.log(process.env.NEXT_PUBLIC_POSTHOG_KEY)
+  console.log(process.env.NEXT_PUBLIC_POSTHOG_HOST)
   const posthogClient = new PostHog(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
     host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
   })
